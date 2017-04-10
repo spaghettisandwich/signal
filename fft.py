5c5c5d6a533102f9402dc4824b609038fe8137d3
Christian Miller <christian.david.miller@gmail.com>
2017-04-10 07:51:25 -0700

    script saves image to file + add image

--- fft.py
+++ fft.py
@@ -1,5 +1,7 @@
 import numpy as np
 from scipy.fftpack import fft
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 print "begin"
@@ -15,7 +17,7 @@ yf = fft(y)
 
 #stuff for x axes
 N = len(lines) #number of samples
-T = 1.0/3000.0 #sample spacing
+T = 1.0/300.0 #sample spacing
 xf = np.linspace(0.0, 1.0/(2.0*T), N//2)
 
 # raw data plot
@@ -27,4 +29,4 @@ xf = np.linspace(0.0, 1.0/(2.0*T), N//2)
 #fft plot
 plt.plot(xf, 2.0/N * np.abs(yf[0:N//2]))
 plt.grid()
-plt.show()
+plt.savefig('fft_python.png')
